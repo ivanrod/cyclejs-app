bccf8ab99c70665ab34353e4cf1d9c642b872c57
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-09-30 22:34:43 +0200

    Add event html backbone

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import {div} from '@cycle/dom'
 import xs from 'xstream'
-import {html} from 'snabbdom-jsx';
+import {html} from 'snabbdom-jsx'
 
 export function App (sources) {
   const vtree$ = xs.of(
-    <div>waitForMe</div>
+    <div>
+      <header>
+        <h1>waitForMe</h1>
+      </header>
+      <article>
+        <h2>Event</h2>
+        <input type="date" name="event-date" />
+        <input type="time" name="event-time" />
+      </article>
+    </div>
   )
   const sinks = {
     DOM: vtree$
