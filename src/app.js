d011abceb1649c8542018f0dbcfb0a22af590cbd
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-09-30 20:47:59 +0200

    Add snabbdom-jsx

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import {div} from '@cycle/dom'
 import xs from 'xstream'
+import {html} from 'snabbdom-jsx';
 
 export function App (sources) {
   const vtree$ = xs.of(
-    div('waitForMe')
+    <div>waitForMe</div>
   )
   const sinks = {
     DOM: vtree$
