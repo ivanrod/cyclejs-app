386b0f19e25cd4b3b6b721332e872fa192ddb804
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-10-01 13:21:43 +0200

    Add event logic with state to app

--- src/app.js
+++ src/app.js
@@ -3,15 +3,32 @@ import xs from 'xstream'
 import {html} from 'snabbdom-jsx'
 
 export function App (sources) {
-  const vtree$ = xs.of(
+  const vtree$ = sources.DOM.select('input').events('change')
+  .map(ev => {
+    const { target: { value, id } } = ev;
+    return {
+      [id]: value
+    }
+  })
+  .fold((acc, next) => {
+
+    return {
+      ...acc,
+      ...next
+    }
+  }, false)
+  .map(({ date, time }) =>
     <div>
       <header>
         <h1>waitForMe</h1>
       </header>
       <article>
         <h2>Event</h2>
-        <input type="date" name="event-date" />
-        <input type="time" name="event-time" />
+        <input id="date" type="date" name="event-date" />
+        <p>Date selected: {date}</p>
+        <input id="time" type="time" name="event-time" />
+        <p>Time selected: {time}</p>
+        <button>Create</button>
       </article>
     </div>
   )
