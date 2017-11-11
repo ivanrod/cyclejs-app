524dbc44a6e31ca8375609c82d7cde8d9b6c62ea
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-11-11 20:04:10 +0100

    Add some styles to EventList

--- src/components/EventList/view.js
+++ src/components/EventList/view.js
@@ -1,20 +1,23 @@
 import {div, ul, li, p} from "@cycle/dom"
-import './styles.css'
+import "./styles.css"
 
-export default ($state) => {
+export default $state => {
   return $state.map(({events}) =>
-    div(
-      [
-        ul(
-          events && Object.keys(events).map(event => li(`.item`, [
-            p(event),
-            p(events[event].date),
-            p(events[event].time),
-            p(events[event].location),
-            p(`.remove`, `X`),
-          ]))
-        ),
-      ]
-    )
+    div([
+      ul(`.event-list`,
+        events && [
+          li(`.item__title .item`, [p(`Name`), p(`Date`), p(`Time`), p(`Location`)]),
+          ...Object.keys(events).map(event =>
+            li(`.item`, [
+              p(event),
+              p(events[event].date),
+              p(events[event].time),
+              p(events[event].location),
+              p(`.remove`, `X`),
+            ])
+          ),
+        ]
+      ),
+    ])
   )
 }
